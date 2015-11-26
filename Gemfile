7e00951c729f20aaa8f4ede10a059e0c3caf6c5f
Driem <gadijaniel@unitec.edu>
2015-11-26 16:36:03 -0600

    Agregando Imagen

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
+
+gem 'paperclip'
 gem 'therubyracer'
 gem 'protected_attributes'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
-attr_accessible  :nombre, :descripcion, :precio, :cantidad
+attr_accessible  :nombre, :descripcion, :precio, :cantidad, :image
 has_many :sells
+
+
+has_attached_file :image, styles: { medium: "600x600", thumb:"300x300"}
+validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
 
